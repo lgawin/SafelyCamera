4640abf6cf9a2dc59ac9fa01066987838056d074
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2020-12-17 22:29:25 +0100

    Add FAB to GalleryFragment

--- app/src/main/java/pl/lgawin/safelycamera/GalleryFragment.kt
+++ app/src/main/java/pl/lgawin/safelycamera/GalleryFragment.kt
@@ -1,5 +1,16 @@
 package pl.lgawin.safelycamera
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import pl.lgawin.safelycamera.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment(R.layout.fragment_gallery)
+class GalleryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        FragmentGalleryBinding.inflate(inflater, container, false)
+            .apply { floatingActionButton.setOnClickListener { toast("Fab clicked") } }
+            .root
+}
--- app/src/main/java/pl/lgawin/safelycamera/ToastKtx.kt
+++ app/src/main/java/pl/lgawin/safelycamera/ToastKtx.kt
@@ -2,5 +2,8 @@ package pl.lgawin.safelycamera
 
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+fun Fragment.toast(message: String) = requireContext().toast(message)
