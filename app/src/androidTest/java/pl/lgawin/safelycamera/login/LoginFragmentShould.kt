a2385cbcde7c1b5db4a6480b2b093645a5cfe8fb
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2020-12-19 18:56:08 +0100

    Login button is disabled at start

---
+++ app/src/androidTest/java/pl/lgawin/safelycamera/login/LoginFragmentShould.kt
@@ -0,0 +1,21 @@
+package pl.lgawin.safelycamera.login
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.CoreMatchers.not
+import org.junit.Test
+import pl.lgawin.safelycamera.R
+
+class LoginFragmentShould {
+
+    @Test
+    fun disableLoginButtonAtStart() {
+        launchFragmentInContainer<LoginFragment>(themeResId = R.style.Theme_SafelyCamera)
+        onView(withText("Login")).check(matches(isDisplayed()))
+            .check(matches(not(isEnabled())))
+    }
+}
