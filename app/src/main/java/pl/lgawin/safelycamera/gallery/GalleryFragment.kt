63f16279c9c5eb95718e7c73d494cf5658c04642
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2020-12-19 09:38:26 +0100

    [poc] Show image from SDCard with CoIL

--- app/src/main/java/pl/lgawin/safelycamera/gallery/GalleryFragment.kt
+++ app/src/main/java/pl/lgawin/safelycamera/gallery/GalleryFragment.kt
@@ -1,16 +1,24 @@
-package pl.lgawin.safelycamera
+package pl.lgawin.safelycamera.gallery
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil.load
 import pl.lgawin.safelycamera.databinding.FragmentGalleryBinding
+import pl.lgawin.safelycamera.toast
+import java.io.File
 
 class GalleryFragment : Fragment() {
 
+    private val path =
+        "/sdcard/Android/data/pl.lgawin.safelycamera/files/Pictures/J20201217_201315_2315534010738133014.jpg"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         FragmentGalleryBinding.inflate(inflater, container, false)
-            .apply { floatingActionButton.setOnClickListener { toast("Fab clicked") } }
+            .apply {
+                image.load(File(path))
+                floatingActionButton.setOnClickListener { toast("Fab clicked") }
+            }
             .root
 }
